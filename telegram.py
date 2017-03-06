9fba9cfdf1dd5775082ec3265c3b591170b54454
Sven <denhartog.sven@gmail.com>
2017-03-06 15:47:20 +0100

    Input checking

--- telegram.py
+++ telegram.py
@@ -9,12 +9,21 @@ import time
 import urllib
 import numpy
 import spacy
+from geotext import GeoText
 # python3: urllib.parse.quote_plus
 # python2: urllib.pathname2url
 
+nlp = spacy.load('en')
+
 TOKEN = "287236464:AAFK-tgprVoLUfSzDl96SkxNK-w7lw77_Lg" # don't put this in your repo! (put in config, then import config)
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-nlp = spacy.load('en')
+textcomp = [u'hello', u'how are you', u'weather in London']
+
+def weather(city):
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=385960ef90069e839464dbf900cbf5ed".format(city)
+    forecast = get_json_from_url(url)
+    #print forecast
+    return forecast
 
 def get_url(url):
     response = requests.get(url)
@@ -49,13 +58,6 @@ def answer_all(updates):
         chat = update["message"]["chat"]["id"]
         send_message(text, chat)
 
-def echo_all(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        send_message(text, chat)
-
-
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
@@ -65,10 +67,18 @@ def get_last_chat_id_and_text(updates):
 
 
 def send_message(text, chat_id):
-    vec = nlp(text)
-    target = nlp(u"What is the weather in Nijmegen?")
-    sim = vec.similarity(target)
-    text = urllib.pathname2url(repr(sim)) # urllib.parse.quote_plus(text) # (python3)
+    doc = nlp(text)
+    places = GeoText(str(text))
+    print places.cities
+    score = []
+    max = 0
+    for i, test in enumerate(textcomp):
+        sim = doc.similarity(nlp(test))
+        score.append(sim)
+        if sim >= score[max]:
+            max = i
+    response = textcomp[max] + ' ' + repr(score[max])
+    text = urllib.pathname2url(response) # urllib.parse.quote_plus(text) # (python3)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
